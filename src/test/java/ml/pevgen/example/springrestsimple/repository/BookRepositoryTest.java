6b71e975befa9d47a24ea06ffa6e32055151d74d
Evgeny Polyak <polyakea@nspk.ru>
2019-11-22 19:26:57 +0300

    Added 'testcontainers' for future test examples

---
+++ src/test/java/ml/pevgen/example/springrestsimple/repository/BookRepositoryTest.java
@@ -0,0 +1,21 @@
+package ml.pevgen.example.springrestsimple.repository;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+class BookRepositoryTest {
+    @Container
+    private static final MySQLContainer MY_SQL_CONTAINER = new MySQLContainer();
+
+    @Test
+    void someTestMethod() {
+        String url = MY_SQL_CONTAINER.getJdbcUrl();
+
+        // create a connection and run test as normal
+        Assert.fail("url:" + url);
+    }
+}
