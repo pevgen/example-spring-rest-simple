a992113b4978e57437c19cb9c09d38909ea5996c
pevgen <pevgen@gmail.com>
2020-01-16 14:18:22 +0300

    google-books-client-test runs without db (only feign-client)

--- src/test/java/ml/pevgen/example/springrestsimple/repository/remote/GoogleBooksClientTest.java
+++ src/test/java/ml/pevgen/example/springrestsimple/repository/remote/GoogleBooksClientTest.java
@@ -3,19 +3,32 @@ package ml.pevgen.example.springrestsimple.repository.remote;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.autoconfigure.http.HttpMessageConvertersAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 
-@SpringBootTest
-@ActiveProfiles("test")
+@SpringBootTest(classes = GoogleBooksClient.class)
+@EnableFeignClients
+@ImportAutoConfiguration({FeignAutoConfiguration.class, HttpMessageConvertersAutoConfiguration.class})
 class GoogleBooksClientTest {
 
     @Autowired
     GoogleBooksClient googleBooksClient;
 
     @Test
-    void findBookByISBN() {
+    void findBookByQuery_byISBN_ok() {
         Results res = googleBooksClient.findBookByQuery("isbn:9780321356680");
         Assertions.assertThat(res).isNotNull();
+        Assertions.assertThat(res.getTotalItems()).isEqualTo(1);
     }
+
+    @Test
+    void findBookByQuery_byISBN_bookNotFound() {
+        Results res = googleBooksClient.findBookByQuery("isbn:0101001010100");
+        Assertions.assertThat(res).isNotNull();
+        Assertions.assertThat(res.getTotalItems()).isEqualTo(0);
+    }
+
 }
